4cf82e3eb6dc669aa2f2e3ba4f61ab864cb4aadd
Francisco José Fernández García <francisco.fernandez@terraminium.com>
2025-01-19 19:33:04 +0100

    add node-fetch

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const https = require('https');
+const fetch = require('node-fetch');
 
 try {
     const issue = github.context.payload.issue;
 
     if (issue.labels.some(label => label.name === 'motivate')) {
-        https.get('https://favqs.com/api/qotd', (resp) => {
-            let data = '';
-
-            // A chunk of data has been received.
-            resp.on('data', (chunk) => {
-            data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-            const response = JSON.parse(data);
-            const quote = response.quote.body;
-            const author = response.quote.author;
-            console.log(`Motivational message: "${quote}" - ${author}`);
-            });
-
-        }).on("error", (err) => {
-            console.error("Error: " + err.message);
-        });
+        fetch('https://favqs.com/api/qotd')
+         .then(response => response.json())
+         .then(data => console.log(`Motivational message: "${data.quote.body}" - ${data.quote.author}`))     
     }
     else {
         console.log('No motivational label found.');
