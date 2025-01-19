228efef4ab8f522bff6fe31ba1e4303b86caecf5
Francisco José Fernández García <francisco.fernandez@terraminium.com>
2025-01-19 18:22:07 +0100

    create github action

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gh-lemon-code-motivational-message
-gh-lemon-code-motivational-message
+This action gets the motivational message when an issue is created with the tag motivate. The console shows the motivational message.
+
+## Inputs
+
+
+## Outputs
+
+
+## Example usage
+
+```yaml
+    - name: Given motivational message
+    uses: franjfgcarmo/get-commodity-price-action@1.
+    with:
+        github-token: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+
+try {
+    const issue = github.context.payload.issue;
+
+    if (issue.labels.some(label => label.name === 'motivate')) {
+        const response = await axios.get('https://favqs.com/api/qotd');
+        const quote = response.data.quote.body;
+        const author = response.data.quote.author;
+        console.log(`Motivational message: "${quote}" - ${author}`);
+    }
+    else {
+        console.log('No motivational label found.');
+    }
+
+} catch (error) {
+    core.setFailed(error.message);
+}
