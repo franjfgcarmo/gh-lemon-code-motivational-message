e72f0d46968be9995e4c08bda66cc33c5c3aaf51
Francisco José Fernández García <francisco.fernandez@terraminium.com>
2025-01-19 18:46:16 +0100

    remove await.

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
-async function run() {
-    try {
-        const issue = github.context.payload.issue;
-    
-        if (issue.labels.some(label => label.name === 'motivate')) {
-            const response = await axios.get('https://favqs.com/api/qotd');
+
+
+try {
+    const issue = github.context.payload.issue;
+
+    if (issue.labels.some(label => label.name === 'motivate')) {
+        axios.get('https://favqs.com/api/qotd').then(response => {
             const quote = response.data.quote.body;
             const author = response.data.quote.author;
             console.log(`Motivational message: "${quote}" - ${author}`);
-        }
-        else {
-            console.log('No motivational label found.');
-        }
-    
-    } catch (error) {
-        core.setFailed(error.message);
+        }).catch(error => {
+            core.setFailed(error.message);
+        });    
     }
-}
+    else {
+        console.log('No motivational label found.');
+    }
 
-run();
+} catch (error) {
+    core.setFailed(error.message);
+}
